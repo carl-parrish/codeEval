ae5237943f41d54ba57431020508162e2a83f09a
Carl Parrish <carl.parrish@gmail.com>
2016-08-03 21:33:07 +0000

    Add js version of fizzbuzz and fix python version

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,18 +1,27 @@
 /*********
  * @source: Eloquent Javascript
- * Write a program that uses console.log to print all the numbers from 1 to 100
+ * Write a program that uses process.stdout.write to print all the numbers from 1 to 100
  * with two exceptions. For numbers divisble by 3, print "Fizz" instead of the 
  * number, and for numbers divisible by 5 (and not 3), print "Buzz"
  * *****************************************************************/
- 
- for(let i=1; i<=100; i++){
-     if((i % 5 == 0 )&& (i % 3 == 0)){
-        console.log("FizzBuzz");
-     }else if(i % 3 == 0){
-         console.log("Fizz");
-     }else if(i % 5 == 0 ){
-         console.log("Buzz");
-     }else{
-         console.log(i);
+ var fs = require("fs");
+ fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function(line) {
+     if(line !=""){
+         let fizz, buzz, max;
+         [fizz, buzz, max] = line.split(' ');
+         let fizzBuzz = fizz * buzz;
+         for(let i = 1; i <= max; i++){
+             if(i % fizzBuzz == 0 ){
+                process.stdout.write("FB ");
+             }else if(i % fizz == 0){
+                 process.stdout.write("F ");
+             }else if(i % buzz == 0 ){
+                 process.stdout.write("B ");
+             }else{
+                 process.stdout.write(i + " ");
+             }
+         }
+         console.log(" ");
      }
- }
+ });
+ 
